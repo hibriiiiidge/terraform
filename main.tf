6dbfaf8f6decd20d6563c229d9f8cdf39fb64d6a
Yasunori Takahashi <hibriiiiidge@gmail.com>
2019-02-25 11:57:50 +0900

    Add configure of aws_autoscaling_group

--- main.tf
+++ main.tf
@@ -5,14 +5,18 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-40d28157"
   instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
   tags {
     Name = "terraform-example"
+  }
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -20,9 +24,36 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
+    from_port = "${var.server_port}"
+    to_port = "${var.server_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default = 8080
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
